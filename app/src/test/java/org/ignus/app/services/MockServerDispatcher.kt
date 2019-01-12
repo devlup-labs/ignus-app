5cd697bbdb9edb9c6afa675a785daa595755fc25
ajatprabha <ajat.prabha.leo@gmail.com>
2019-01-13 01:06:01 +0530

    refactor MockServerDispatcher

--- app/src/test/java/org/ignus/app/services/MockServerDispatcher.kt
+++ app/src/test/java/org/ignus/app/services/MockServerDispatcher.kt
@@ -4,19 +4,22 @@ import com.google.gson.Gson
 import okhttp3.mockwebserver.Dispatcher
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.RecordedRequest
+import org.ignus.app.db.models.LoginResponse
 import org.ignus.app.mockdata.VALID_JWT_TOKEN
 import org.ignus.app.mockdata.eventCategoryList
 
 class MockServerDispatcher : Dispatcher() {
     override fun dispatch(request: RecordedRequest?): MockResponse {
-        if (request != null) {
-            return when (request.path) {
-                "/token-auth/" -> MockResponse().setResponseCode(200).setBody("{\"token\":\"$VALID_JWT_TOKEN\"}")
-                "/event/category-list/" -> MockResponse().setResponseCode(200).setBody(Gson().toJson(eventCategoryList))
+        return if (request != null) {
+            when (request.path) {
+                "/token-auth/" -> MockResponse()
+                        .setResponseCode(200)
+                        .setBody(Gson().toJson(LoginResponse(VALID_JWT_TOKEN)))
+                "/event/category-list/" -> MockResponse()
+                        .setResponseCode(200)
+                        .setBody(Gson().toJson(eventCategoryList))
                 else -> MockResponse().setResponseCode(404)
             }
-        }
-
-        return MockResponse().setResponseCode(404)
+        } else MockResponse().setResponseCode(404)
     }
 }

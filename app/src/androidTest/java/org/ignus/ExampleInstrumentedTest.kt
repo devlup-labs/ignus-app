69cb25546e4034c80f9e27465d81fb5df4d0a49a
abhinavsuthar <developer.me.abhi@gmail.com>
2019-01-31 12:02:32 +0530

    test

---
+++ app/src/androidTest/java/org/ignus/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package org.ignus
+
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented layout, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under layout.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("org.ignus", appContext.packageName)
+    }
+}

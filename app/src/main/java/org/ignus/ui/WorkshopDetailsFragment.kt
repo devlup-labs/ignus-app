86726cfb79d94c1af3211af9d150d7938296fdf3
abhinavsuthar <developer.me.abhi@gmail.com>
2019-01-31 12:19:19 +0530

    firest commit

---
+++ app/src/main/java/org/ignus/ui/WorkshopDetailsFragment.kt
@@ -0,0 +1,44 @@
+package org.ignus.ui
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import org.ignus.R
+
+class WorkshopDetailsFragment : BottomSheetDialogFragment() {
+
+    companion object {
+        fun newInstance(): WorkshopDetailsFragment {
+            return WorkshopDetailsFragment()
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.event_categories_card_2, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+//        val bottomSheetNavFragment = WorkshopsFragment()
+//        childFragmentManager.beginTransaction()
+        Log.d("suthar", "onViewCrated")
+
+    }
+
+//    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+//        return object : BottomSheetDialog(activity!!, theme) {
+//            override fun onBackPressed() {
+//                val fragment = childFragmentManager.fragments[0]
+//                val navigateUp = findNavController(fragment.view!!).navigateUp()
+//                if (!navigateUp) {
+//                    dismiss()
+//                }
+//            }
+//        }
+//    }
+
+}

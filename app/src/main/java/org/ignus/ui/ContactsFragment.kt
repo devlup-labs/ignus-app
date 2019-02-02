887eb1851170b86eb793778ab68fc2efc0af8edf
abhinavsuthar <developer.me.abhi@gmail.com>
2019-02-03 00:47:55 +0530

    added web-view for contacts and sponsors page

--- app/src/main/java/org/ignus/ui/ContactsFragment.kt
+++ app/src/main/java/org/ignus/ui/ContactsFragment.kt
@@ -5,7 +5,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_contacts.*
 import org.ignus.R
 
 
@@ -15,5 +19,29 @@ class ContactsFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_contacts, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpWebView()
+    }
 
+    private fun setUpWebView() {
+
+        val wb = contactsWebView
+        wb.settings.javaScriptEnabled = true
+        wb.settings.loadWithOverviewMode = true
+        wb.settings.useWideViewPort = true
+        wb.loadUrl("https://ignus.org/team/")
+
+        wb.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                contactsProgressBar.visibility = View.GONE
+            }
+
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                view?.loadUrl("https://ignus.org/team/")
+                return true
+            }
+        }
+    }
 }
--- app/src/main/java/org/ignus/ui/SponsorsFragment.kt
+++ app/src/main/java/org/ignus/ui/SponsorsFragment.kt
@@ -5,7 +5,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_sponsors.*
 import org.ignus.R
 
 
@@ -15,5 +19,29 @@ class SponsorsFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_sponsors, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpWebView()
+    }
 
+    private fun setUpWebView() {
+
+        val wb = sponsorsWebView
+        wb.settings.javaScriptEnabled = true
+        wb.settings.loadWithOverviewMode = true
+        wb.settings.useWideViewPort = true
+        wb.loadUrl("https://ignus.org/sponsors/")
+
+        wb.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                sponsorsProgressBar.visibility = View.GONE
+            }
+
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                view?.loadUrl("https://ignus.org/sponsors/")
+                return true
+            }
+        }
+    }
 }

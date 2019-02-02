2513445303905be4f4053b6acf9d4eb778cb8b65
abhinavsuthar <developer.me.abhi@gmail.com>
2019-02-03 02:02:05 +0530

    push notification in foreground

--- app/src/main/java/org/ignus/services/MyFirebaseMessagingService.kt
+++ app/src/main/java/org/ignus/services/MyFirebaseMessagingService.kt
@@ -1,16 +1,46 @@
 package org.ignus.services
 
+import android.R
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.util.Log
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import org.ignus.ui.MainActivity
 
-class MyFirebaseMessagingService:FirebaseMessagingService() {
 
-    override fun onMessageReceived(p0: RemoteMessage?) {
-        super.onMessageReceived(p0)
+class MyFirebaseMessagingService : FirebaseMessagingService() {
 
-        if (p0?.notification != null) {
-            Log.d("suthar", "Message Notification Body: " + p0.notification?.body)
+    override fun onMessageReceived(remoteMessage: RemoteMessage?) {
+        super.onMessageReceived(remoteMessage)
+
+        if (remoteMessage?.notification != null) {
+            Log.d("suthar", "Message Notification Body: " + remoteMessage.notification?.body)
+
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+            val channelId = "Default"
+
+            val builder = NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(R.mipmap.sym_def_app_icon)
+                .setContentTitle(remoteMessage.notification?.title)
+                .setContentText(remoteMessage.notification?.body)
+                .setStyle(NotificationCompat.BigTextStyle().bigText(remoteMessage.notification?.body))
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val channel = NotificationChannel(channelId, "Default channel", NotificationManager.IMPORTANCE_DEFAULT)
+                manager.createNotificationChannel(channel)
+            }
+            manager.notify(0, builder.build())
         }
     }
 }

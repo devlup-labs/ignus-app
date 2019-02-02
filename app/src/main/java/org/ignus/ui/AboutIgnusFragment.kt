8b5deb862d6729dbc75ebf727c5788d5ead81bb1
abhinavsuthar <developer.me.abhi@gmail.com>
2019-02-03 01:55:34 +0530

    added about us web-view

--- app/src/main/java/org/ignus/ui/AboutIgnusFragment.kt
+++ app/src/main/java/org/ignus/ui/AboutIgnusFragment.kt
@@ -5,7 +5,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_about_ignus.*
 import org.ignus.R
 
 
@@ -15,5 +19,30 @@ class AboutIgnusFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_about_ignus, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpWebView()
+    }
+
+    private fun setUpWebView() {
+
+        val wb = aboutWebView
+        wb.settings.javaScriptEnabled = true
+        wb.settings.loadWithOverviewMode = true
+        wb.settings.useWideViewPort = true
+        wb.loadUrl("https://ignus.org/#about_us")
+
+        wb.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                aboutProgressBar.visibility = View.GONE
+            }
+
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                view?.loadUrl("https://ignus.org/#about_us")
+                return true
+            }
+        }
+    }
 
 }

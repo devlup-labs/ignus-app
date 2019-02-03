79e8c25e7dc474991c012550b7bfe5d641e0fb14
abhinavsuthar <developer.me.abhi@gmail.com>
2019-02-04 01:00:39 +0530

    added login and developers fragment

---
+++ app/src/main/java/org/ignus/ui/DevelopersFragment.kt
@@ -0,0 +1,22 @@
+package org.ignus.ui
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.ignus.R
+
+
+class DevelopersFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_developers, container, false)
+    }
+
+
+}

5cfaad2f5a19540229c476a02b5bef17e6e9405b
abhinavsuthar <developer.me.abhi@gmail.com>
2019-02-10 16:11:01 +0530

    Some Login page tweaks

--- app/src/main/java/org/ignus/ui/LoginFragment.kt
+++ app/src/main/java/org/ignus/ui/LoginFragment.kt
@@ -6,11 +6,14 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.fragment_login.*
 import org.ignus.R
 import org.ignus.db.viewmodels.LoginVM
+
 
 class LoginFragment : Fragment() {
 
@@ -23,7 +26,35 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        refreshUserProfile("IG-ABH-737", "password-password")
+
+        igNumberEditText.addTextChangedListener {
+            val text = it.toString()
+            val length = it.toString().length
+
+            if (text.endsWith(" ")) {
+
+            }
+
+            if (length == 2 || length == 6) {
+                igNumberEditText.setText(StringBuilder(text).insert(text.length, "-").toString())
+                igNumberEditText.setSelection(igNumberEditText.text.toString().length)
+            }
+
+            loginButton.isEnabled = !(it.isNullOrBlank() || passwordEditText.text.isNullOrBlank())
+        }
+
+        passwordEditText.addTextChangedListener {
+            loginButton.isEnabled = !(it.isNullOrBlank() || igNumberEditText.text.isNullOrBlank())
+        }
+
+        loginButton.setOnClickListener {
+            val igNumber = igNumberEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            refreshUserProfile(igNumber, password)
+        }
+
+        // refreshUserProfile("IG-ABH-737", "password-password")
         consumeProfile()
     }
 

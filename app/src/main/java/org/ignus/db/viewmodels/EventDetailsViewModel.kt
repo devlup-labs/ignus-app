1ec3f95b080e7d92d93743e2c8704a1d119e160a
abhinavsuthar <developer.me.abhi@gmail.com>
2019-02-12 23:40:25 +0530

    bug fixed when clicked multiple events in EventDetailsFragment

--- app/src/main/java/org/ignus/db/viewmodels/EventDetailsViewModel.kt
+++ app/src/main/java/org/ignus/db/viewmodels/EventDetailsViewModel.kt
@@ -1,6 +1,5 @@
 package org.ignus.db.viewmodels
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import io.reactivex.observers.DisposableObserver

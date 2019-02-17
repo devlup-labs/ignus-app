4240fa5dc1b2fa8fae6004e60339c5a0c76385c8
abhinavsuthar <developer.me.abhi@gmail.com>
2019-02-17 23:24:59 +0530

    added IGMUN page

---
+++ app/src/main/java/org/ignus/ui/IgmunFragment.kt
@@ -0,0 +1,46 @@
+package org.ignus.ui
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebResourceRequest
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_igmun.*
+import org.ignus.R
+
+
+class IgmunFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_igmun, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpWebView()
+    }
+
+    private fun setUpWebView() {
+
+        val url = "https://ignus.org/events/mun/"
+        val wb = igmunWebView
+        wb.settings.javaScriptEnabled = true
+        wb.settings.useWideViewPort = true
+        wb.settings.cacheMode = WebSettings.LOAD_NO_CACHE
+        wb.loadUrl(url)
+
+        wb.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                if (this@IgmunFragment.isVisible) igmunProgressBar.visibility = View.GONE
+            }
+        }
+    }
+
+
+}
